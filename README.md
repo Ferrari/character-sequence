4daa7473a0c0bafd63d22b54b8e1b6b73618c31b
Ferrari Lee <shiyung@gmail.com>
2016-10-06 13:36:46 +0800

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# character-sequence
+
+Small toolkit can count word sequence and do something else
+
+## Getting started
+
+Install module from npm first:
+```
+npm install character-sequence
+```
+
+## Basic Usage
+
+```
+var CharacterSequence = require('character-sequence')
+
+var count1 = CharacterSequence('test')
+// count1: [ 19, 4, 18, 19 ]
+
+var count2 = CharacterSequence('small')
+// count2: [ 18, 12, 0, 11, 11 ]
+var count3 = CharacterSequence('small', { positive: true })
+// count3: [ 19, 13, 1, 12, 12 ]
+
+var count4 = CharacterSequence('test', { merge: true })
+// count4: 60
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const CHAR_ARRAY = CHAR.split('')
+
+function _countCharSeq (word, onlyPositive) {
+  let wordArr = word.toUpperCase().split('')
+  let countArr = wordArr.map((char) => {
+    return (onlyPositive === true)
+      ? (CHAR_ARRAY.indexOf(char) + 1)
+      : CHAR_ARRAY.indexOf(char)
+  })
+
+  return countArr
+}
+
+function CharacterSequence (word, opts) {
+  if (!this instanceof CharacterSequence) {
+    return new CharacterSequence(word)
+  }
+
+  if (typeof word !== 'string') {
+    throw new TypeError('Input text must be string')
+  }
+
+  let countArr = (opts && opts.positive === true)
+    ? _countCharSeq(word, true)
+    : _countCharSeq(word, false)
+
+  return (opts && opts.merge === true)
+  ? countArr.reduce((prev, curr) => { return (prev += curr) })
+  : countArr
+}
+
+module.exports = CharacterSequence
